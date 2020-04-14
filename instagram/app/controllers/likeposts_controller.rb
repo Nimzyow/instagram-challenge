2dc1674d47bcf5bf504e2045d4ba13af427f4de9
nimzyow <n_soufiani@hotmail.com>
2020-04-14 23:01:01 +0100

    can unlike post

--- instagram/app/controllers/likeposts_controller.rb
+++ instagram/app/controllers/likeposts_controller.rb
@@ -2,11 +2,21 @@ class LikepostsController < ApplicationController
   def create
     @likepost = Likepost.where(post_id: params[:post_id], user_id: current_user.id)
     if @likepost.length === 0
-      Likepost.create(user_id: current_user.id , post_id: params[:post_id])
-      redirect_to root_path
+      like_post
     else
-      Likepost.delete(@likepost)
-      redirect_to root_path
+      unlike_post
     end
+  end
+
+  private 
+
+  def like_post
+    Likepost.create(user_id: current_user.id , post_id: params[:post_id])
+    redirect_to root_path
+  end
+
+  def unlike_post
+    Likepost.delete(@likepost)
+    redirect_to root_path
   end
 end
